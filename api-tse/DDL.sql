e871784ea28504bb8afcfda80053b9118cacc38f
JoseBenja <jose.benjamin.mendez@hotmail.com>
2023-08-28 21:21:09 -0600

    DDL y DML

---
+++ api-tse/DDL.sql
@@ -0,0 +1,24 @@
+CREATE TABLE tse.catalogo (
+                              id_catag serial4 NOT NULL,
+                              tipo varchar(20) NOT NULL,
+                              estado bool NOT NULL DEFAULT true,
+                              codigo varchar(5) NOT NULL,
+                              nombre varchar(100) NOT NULL,
+                              referencia_id_catag int4 NULL,
+                              CONSTRAINT catalogo_pkey PRIMARY KEY (id_catag),
+                              CONSTRAINT fk_id_catag_catalogo FOREIGN KEY (referencia_id_catag) REFERENCES tse.catalogo(id_catag)
+);
+
+CREATE TABLE tse.votante (
+                             no_empadronado int4 NOT NULL DEFAULT nextval('tse.votante_no_empradonado_seq'::regclass),
+                             cui varchar(13) NOT NULL,
+                             municipio int4 NOT NULL,
+                             direccion varchar(100) NOT NULL,
+                             voto_emitido bool NOT NULL DEFAULT false,
+                             CONSTRAINT votante_pkey PRIMARY KEY (no_empadronado)
+);
+
+
+-- tse.votante foreign keys
+
+ALTER TABLE tse.votante ADD CONSTRAINT fk_municipio_catalogo FOREIGN KEY (municipio) REFERENCES tse.catalogo(id_catag);
